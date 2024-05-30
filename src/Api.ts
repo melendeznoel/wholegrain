27be700d07e4b80a426455dde77fa8a79c93ce7f
oatcoder <noelmelendez@gmail.com>
2024-05-30 12:16:48 -0500

    update
    re-org project structure
    fixed index
    added missing scripts

--- src/Api.ts
+++ src/Api.ts
@@ -1,12 +1,13 @@
-import express, { NextFunction, Request, Response, Express } from 'express'
-import { Routing } from './Routing'
+import express, { NextFunction, Request, Response, Express, Router } from 'express'
 
 
 export class Api {
   public instance: Express
+  public routing: Router
 
   constructor () {
     this.instance = express()
+    this.routing = Router()
     /** Parse the request */
     this.instance.use(express.urlencoded({ extended: false }))
     /** Takes care of JSON data */
@@ -22,13 +23,16 @@ export class Api {
     this.instance.use((req: Request, res: Response, next: NextFunction) => {
       // set the CORS policy
       res.header('Access-Control-Allow-Origin', '*')
+
       // set the CORS headers
       res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization')
+
       // set the CORS method headers
       if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST')
         return res.status(200).json({})
       }
+
       next()
     })
   }
@@ -44,6 +48,10 @@ export class Api {
   }
 
   public setRoutes(){
-    this.instance.use('/', new Routing().router)
+    this.routing.get('/info', (req: Request, res: Response, next: NextFunction) => {
+      res.status(200).json({ name: 'info', description: 'dev api' })
+    })
+
+    this.instance.use('/', this.routing)
   }
 }
