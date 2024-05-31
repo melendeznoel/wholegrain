0dae21361a9a321c16902c2b47bba0faf39c844e
oatcoder <noelmelendez@gmail.com>
2024-05-31 16:00:51 -0500

    update
    - api port

--- src/Index.ts
+++ src/Index.ts
@@ -2,11 +2,11 @@ import { logger } from './logging'
 
 import { Api } from './Api'
 
-const port = (process.env.PORT != null) || '3001'
+const port = '80'
 
 logger.info('Starting API')
 
 new Api().instance.listen(port, () => {
-  logger.info(`API listening on ${port}`)
+  logger.info(`Started API listening on ${port}`)
   return true
 })
