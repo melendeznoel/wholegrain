c4749abe99c7741288d8b6c1bf5da308a8f7322e
oatcoder <noelmelendez@gmail.com>
2024-06-01 17:21:27 -0500

    update
    - removed port var

--- src/Index.ts
+++ src/Index.ts
@@ -2,11 +2,9 @@ import { logger } from './logging'
 
 import { Api } from './Api'
 
-const port = 443
-
 logger.info('Starting API')
 
-new Api().instance.listen(port, () => {
-  logger.info(`Started API listening on ${port}`)
+new Api().instance.listen(3001, () => {
+  logger.info('Started API')
   return true
 })
