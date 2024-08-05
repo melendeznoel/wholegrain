bae47d8bcee1dddb2776631d5c6b2adf2e50c829
oatcoder <noelmelendez@gmail.com>
2024-08-04 19:11:31 -0500

    UPDATE
    - added sinon for mocking

--- test/grain/VisionService.test.ts
+++ test/grain/VisionService.test.ts
@@ -1,17 +1,26 @@
 import { VisionService } from '../../src/grain'
 import test, { beforeEach, describe } from 'node:test'
-import { strictEqual } from 'node:assert'
+import { strictEqual, ok } from 'node:assert'
 import { logger } from '../../src/logging'
+import { spy } from 'sinon'
 
-describe('run function', () => {
+describe('VisionService', () => {
   let visionService: VisionService
+  let logInfoSpy: any
 
   beforeEach(() => {
     visionService = new VisionService(logger, {})
+
+    logInfoSpy = spy(logger, 'info')
   })
 
-  test('should return true when the user is logged in', () => {
-    const result = visionService.run()
-    strictEqual(result, true, 'returns true when the user is logged in')
+  describe('run function', () => {
+    test('should return true when the user is logged in', () => {
+      const result = visionService.run()
+
+      ok(logInfoSpy.calledOnceWith('Running Vision Service'))
+
+      strictEqual(result, true, 'returns true when the user is logged in')
+    })
   })
 })
